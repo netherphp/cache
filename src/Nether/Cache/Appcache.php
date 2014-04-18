b7d09471de5c8b73fe9216e4215de69479bbef1e
Bob Majdak Jr <bob@majdak.net>
2014-04-18 05:41:37 -0500

    cache drivers!

---
+++ src/Nether/Cache/Appcache.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Nether\Cache;
+use \Nether;
+
+class Appcache extends DriverInterface {
+
+	protected $Storage = [];
+	/*//
+	@type array
+	where the appcache will store its data.
+	//*/
+
+	////////////////
+	////////////////
+
+	public function Drop($key) {
+		if(array_key_exists($key,$this->Storage)) {
+			unset($this->Storage[$key]);
+		}
+
+		return;
+	}
+
+	public function Flush() {
+		unset($this->Storage);
+		$this->Storage = [];
+
+		return;
+	}
+
+	public function Get($key) {
+		if(array_key_exists($key,$this->Storage)) {
+			++$this->Hit;
+			return $this->Storage[$key];
+		} else {
+			++$this->Miss;
+			return;
+		}
+	}
+
+	public function Set($key,$value) {
+		$this->Storage[$key] = $value;
+
+		return;
+	}
+
+}
---
+++ src/Nether/Cache/Memcache.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Nether\Cache;
+use \Nether;
+
+Nether\Option::Define([
+	'cache-memcache-pool' => []
+]);
+
+class Memcache extends DriverInterface {
+
+	protected $Driver;
+	/*//
+	@type object
+	this is an instance of the Memcache driver from the PHP Memcache Extension
+	for us to interact with the network pool.
+	see: http://us1.php.net/manual/en/class.memcache.php
+	//*/
+
+	////////////////
+	////////////////
+
+	public function __construct() {
+		$this->Driver = new \Memcache;
+
+		$pool = Nether\Option::Get('cache-memcache-pool');
+
+		/*//todo/
+		log instead of throw exception.
+		//*/
+
+		if(!is_array($pool))
+		throw new \Exception('cache-memcache-pool is not an array');
+
+		foreach($pool as $server)
+		$this->AddServer($server);
+
+		return;
+	}
+
+	public function AddServer($server) {
+		if(strpos($server,':')) {
+			list($host,$port) = explode(':',$server);
+		} else {
+			$host = $server;
+			$port = 11211;
+		}
+
+		/*//todo/
+		log instead of throw exception.
+		//*/
+
+		if(!$this->Driver->addServer($host,$port))
+		throw new \Exception("Error adding {$host} to Memcache pool.");
+
+		return;
+	}
+
+	////////////////
+	////////////////
+
+	public function Drop($key) {
+		if(!is_object($this->Driver)) return;
+
+		$this->Driver->delete($key);
+
+		return;
+	}
+
+	public function Flush() {
+		if(!is_object($this->Driver)) return;
+
+		$this->Driver->flush();
+
+		return;
+	}
+
+	public function Get($key) {
+		if(!is_object($this->Driver)) return;
+
+		$value = $this->Driver->get($key);
+		if($value === false) {
+			++$this->Miss;
+			return null;
+		} else {
+			++$this->Hit;
+			return unserialize($value);
+		}
+	}
+
+	public function Set($key,$value) {
+		if(!is_object($this->Driver)) return;
+
+		$this->Driver->set($key,serialize($value));
+
+		return;
+	}
+
+}
